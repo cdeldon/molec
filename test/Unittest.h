4c90a96840cb99f742afe835d4f879e2cbe83a2d
thfabian <fabian_thuering@hotmail.com>
2016-03-17 20:27:08 +0100

    Adding Unittesting framework (based on tinytest: https://github.com/thfabian/tinytest) and a proper LICENCE.txt

---
+++ test/Unittest.h
@@ -0,0 +1,21 @@
+/*                   _
+ *   _ __ ___   ___ | | ___  ___
+ *  | '_ ` _ \ / _ \| |/ _ \/ __|
+ *  | | | | | | (_) | |  __/ (__
+ *  |_| |_| |_|\___/|_|\___|\___| - Molecular Dynamics Framework
+ *
+ *  Copyright (C) 2016  Carlo Del Don  (deldonc@student.ethz.ch)
+ *                      Michel Breyer  (mbreyer@student.ethz.ch)
+ *                      Florian Frei   (frofrei@student.ethz.ch)
+ *                      Fabian Thuring (thfabian@student.ethz.ch)
+ *
+ *  This file is distributed under the MIT Open Source License.
+ *  See LICENSE.txt for details.
+ */
+ 
+#ifndef MOLEC_UNITTEST_H
+#define MOLEC_UNITTEST_H
+
+#include "external/tinytest.h"
+
+#endif
---
+++ test/UnittestMain.c
@@ -0,0 +1,34 @@
+/*                   _
+ *   _ __ ___   ___ | | ___  ___
+ *  | '_ ` _ \ / _ \| |/ _ \/ __|
+ *  | | | | | | (_) | |  __/ (__
+ *  |_| |_| |_|\___/|_|\___|\___| - Molecular Dynamics Framework
+ *
+ *  Copyright (C) 2016  Carlo Del Don  (deldonc@student.ethz.ch)
+ *                      Michel Breyer  (mbreyer@student.ethz.ch)
+ *                      Florian Frei   (frofrei@student.ethz.ch)
+ *                      Fabian Thuring (thfabian@student.ethz.ch)
+ *
+ *  This file is distributed under the MIT Open Source License.
+ *  See LICENSE.txt for details.
+ */
+
+#define TINYTEST_MAIN
+#include "Unittest.h"
+
+/**
+ * Setup the Unittest environment
+ *
+ * For documentaion see https://github.com/thfabian/tinytest
+ */
+int main(int argc, char* argv[])
+{
+    tinytest_init(argc, argv);
+
+    // Register new testcases here
+    REGISTER_TEST_CASE(molec_UnittestTimer);
+
+    tinytest_run();
+    tinytest_free();
+    return 0;
+}
