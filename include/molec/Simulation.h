67fe19b441f250d11d478698bc60657259ee67db
Michel Breyer <michel.p.breyer@gmail.com>
2016-03-30 16:06:09 +0200

    helper functions for allocating and freeing memory of simulation_SOA structs

--- include/molec/Simulation.h
+++ include/molec/Simulation.h
@@ -20,7 +20,7 @@
 
 /**
  * @brief Struct of arrays of the position, velocity and force arrays
- * 
+ *
  * The size of the arrays can be queried using molec_parameter->N.
  */
 typedef struct molec_Simulation_SOA
@@ -43,6 +43,16 @@ typedef struct molec_Simulation_SOA
 } molec_Simulation_SOA_t;
 
 /**
+ * Return a pointer to an allocated simulation SOA
+ */
+molec_Simulation_SOA_t* molec_init_simulation_SOA();
+
+/**
+ * Free memory of a simulation SOA
+ */
+void molec_free_simulation_SOA(molec_Simulation_SOA_t* simulation);
+
+/**
  * Print the current state of the simulation
  */
 void molec_print_simulation_SOA(const molec_Simulation_SOA_t* simulation);
@@ -54,4 +64,3 @@ void molec_print_simulation_SOA(const molec_Simulation_SOA_t* simulation);
 void molec_run_simulation(void (*molec_compute_force)( molec_Simulation_SOA_t*, Real*, int));
 
 #endif
-
