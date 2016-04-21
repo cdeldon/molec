656fd0e6031b5e06638504abbe91e31aa6c0ae2d
cdeldon <carlo.deldon@gmail.com>
2016-04-21 10:54:07 +0200

    fixed sorting?

--- src/Sort.c
+++ src/Sort.c
@@ -21,14 +21,23 @@ int molec_compare(const void* pair1, const void* pair2)
 {
     Real pair1_key = (Real) ((molec_Sort_Pair_t*) pair1)->key;
     Real pair2_key = (Real) ((molec_Sort_Pair_t*) pair2)->key;
+
+    int pair1_value = (int)((molec_Sort_Pair_t*) pair1)->value;
+    int pair2_value = (int)((molec_Sort_Pair_t*) pair2)->value;
     
     // Equalilty will never happen
     if(pair1_key < pair2_key)
         return -1;
     else if(pair1_key > pair2_key)
         return 1;
-    else
-        return 0;
+    else // pair1_key == pair2_key
+    {
+        if(pair1_value < pair2_value)
+            return -1;
+        else
+            return 1;
+    }
+
 }
 
 void molec_sort_qsort(molec_Simulation_SOA_t* sim)
