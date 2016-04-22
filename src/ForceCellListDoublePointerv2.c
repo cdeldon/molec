92a52c93b29497eba0fba1685e7e16983041cc8f
Florian Frei <flofrei@student.ethz.ch>
2016-04-22 13:31:26 +0200

    formating and remove warning

--- src/ForceCellListDoublePointerv2.c
+++ src/ForceCellListDoublePointerv2.c
@@ -35,40 +35,39 @@ MOLEC_INLINE int mod(int b, int m)
 /**
   * Transform cellNumber to respective 1D indices
   */
-MOLEC_INLINE void molec_1to3trans(int cellNumber,int* idx_x,int* idx_y,int* idx_z)
+MOLEC_INLINE void molec_1to3trans(int cellNumber, int* idx_x, int* idx_y, int* idx_z)
 {
-    *idx_z=cellNumber / (cellList_parameter.N_x*cellList_parameter.N_y);
-    *idx_y=cellNumber / cellList_parameter.N_x - cellList_parameter.N_y*(*idx_z);
-    *idx_x=cellNumber - cellList_parameter.N_x * (*idx_y + cellList_parameter.N_y * *idx_z);
+    *idx_z = cellNumber / (cellList_parameter.N_x * cellList_parameter.N_y);
+    *idx_y = cellNumber / cellList_parameter.N_x - cellList_parameter.N_y*(*idx_z);
+    *idx_x = cellNumber - cellList_parameter.N_x*(*idx_y + cellList_parameter.N_y * *idx_z);
 }
 
 /**
   * Transform 1D indices to respective cellNumber
   */
-MOLEC_INLINE void molec_3to1trans(int idx_x,int idx_y,int idx_z,int* cellNumber)
+MOLEC_INLINE void molec_3to1trans(int idx_x, int idx_y, int idx_z, int* cellNumber)
 {
-    *cellNumber=idx_x + cellList_parameter.N_x * (idx_y + cellList_parameter.N_y * idx_z);
+    *cellNumber = idx_x + cellList_parameter.N_x*(idx_y + cellList_parameter.N_y * idx_z);
 }
 
 /**
   * Calculate neighbors
   */
-MOLEC_INLINE void molec_get_neighbors(int cellNumber)
+MOLEC_INLINE void molec_get_neighbors(int cellNumber, int* neighbors)
 {
-    int idx_x,idx_y,idx_z;
-    int index=0;
-    molec_1to3trans(cellNumber,&idx_x,&idx_y,&idx_z);
+    int idx_x, idx_y, idx_z;
+    int index = 0;
+    molec_1to3trans(cellNumber, &idx_x, &idx_y, &idx_z);
     for(int d_z = -1; d_z <= 1; ++d_z)
-    for(int d_y = -1; d_y <= 1; ++d_y)
-    for(int d_x = -1; d_x <= 1; ++d_x)
-    {
-        int n_idx_z = mod(idx_z + d_z, cellList_parameter.N_z);
-        int n_idx_y = mod(idx_y + d_y, cellList_parameter.N_y);
-        int n_idx_x = mod(idx_x + d_x, cellList_parameter.N_x);
-        molec_3to1trans(n_idx_x,n_idx_y,n_idx_z,&neighbors[index]);
-        ++index;
-    }
-
+        for(int d_y = -1; d_y <= 1; ++d_y)
+            for(int d_x = -1; d_x <= 1; ++d_x)
+            {
+                int n_idx_z = mod(idx_z + d_z, cellList_parameter.N_z);
+                int n_idx_y = mod(idx_y + d_y, cellList_parameter.N_y);
+                int n_idx_x = mod(idx_x + d_x, cellList_parameter.N_x);
+                molec_3to1trans(n_idx_x, n_idx_y, n_idx_z, &neighbors[index]);
+                ++index;
+            }
 }
 
 /**
@@ -87,21 +86,21 @@ MOLEC_INLINE Real dist(Real x, Real y, Real L)
 void molec_force_cellList_double_pointer_v2(molec_Simulation_SOA_t* sim, Real* Epot, const int N)
 {
     assert(molec_parameter);
-    //get molec parameter
+    // get molec parameter
     cellList_parameter = molec_parameter->cellList;
-    //get average
-    average_atoms_per_cells=molec_parameter->N/cellList_parameter.N;
-    //increase expectation of atoms in cell
-    increased_average=2*average_atoms_per_cells;
+    // get average
+    average_atoms_per_cells = molec_parameter->N / cellList_parameter.N;
+    // increase expectation of atoms in cell
+    increased_average = 2 * average_atoms_per_cells;
 
     int index;
-    //allocation of space for cellList
-    if(molec_cellList==NULL)
+    // allocation of space for cellList
+    if(molec_cellList == NULL)
     {
-        MOLEC_MALLOC(molec_cellList,cellList_parameter.N*sizeof(int*));
-        for(index = 0;index<cellList_parameter.N;++index)
+        MOLEC_MALLOC(molec_cellList, cellList_parameter.N * sizeof(int*));
+        for(index = 0; index < cellList_parameter.N; ++index)
         {
-        MOLEC_MALLOC(molec_cellList[index],increased_average*sizeof(int));
+            MOLEC_MALLOC(molec_cellList[index], increased_average * sizeof(int));
         }
     }
 
@@ -120,175 +119,174 @@ void molec_force_cellList_double_pointer_v2(molec_Simulation_SOA_t* sim, Real* E
 
     Real Epot_ = 0;
 
-    //number of atoms in the cell[cellnumber]
+    // number of atoms in the cell[cellnumber]
     int* NumberOfAtomsInCell;
-    MOLEC_MALLOC(NumberOfAtomsInCell,sizeof(int)*cellList_parameter.N);
-    //set number to zero in each cell
-    memset(NumberOfAtomsInCell, 0, sizeof(int)*cellList_parameter.N);
+    MOLEC_MALLOC(NumberOfAtomsInCell, sizeof(int) * cellList_parameter.N);
+    // set number to zero in each cell
+    memset(NumberOfAtomsInCell, 0, sizeof(int) * cellList_parameter.N);
 
-    int* CellIndexOfAtom;//storing corresponding cellnumber
-    MOLEC_MALLOC(CellIndexOfAtom,sizeof(int)*molec_parameter->N);
+    int* CellIndexOfAtom; // storing corresponding cellnumber
+    MOLEC_MALLOC(CellIndexOfAtom, sizeof(int) * molec_parameter->N);
 
 
-    for(int i = 0; i < N; ++i) //structure the celllist
+    for(int i = 0; i < N; ++i) // structure the celllist
     {
         // linear one dimensional index of cell associated to i-th particle
         int idx_x = x[i] / cellList_parameter.c_x;
         int idx_y = y[i] / cellList_parameter.c_y;
         int idx_z = z[i] / cellList_parameter.c_z;
 
-        //set forces to zero
+        // set forces to zero
         f_x[i] = f_y[i] = f_z[i] = 0.0;
 
         // linear index of cell
         int cellNmbr;
-        molec_3to1trans(idx_x,idx_y,idx_z,&cellNmbr);
-        CellIndexOfAtom[i]=cellNmbr;
+        molec_3to1trans(idx_x, idx_y, idx_z, &cellNmbr);
+        CellIndexOfAtom[i] = cellNmbr;
 
-        molec_cellList[cellNmbr][NumberOfAtomsInCell[cellNmbr]]=i; //segfault?
-        NumberOfAtomsInCell[cellNmbr]+=1;
-        assert(NumberOfAtomsInCell[cellNmbr]<=increased_average);
+        molec_cellList[cellNmbr][NumberOfAtomsInCell[cellNmbr]] = i; // segfault?
+        NumberOfAtomsInCell[cellNmbr] += 1;
+        assert(NumberOfAtomsInCell[cellNmbr] <= increased_average);
     }
 
     int CellNumberOne;
-    //loop over all cells
-    for(CellNumberOne=0;CellNumberOne<cellList_parameter.N;++CellNumberOne)
+    // loop over all cells
+    for(CellNumberOne = 0; CellNumberOne < cellList_parameter.N; ++CellNumberOne)
     {
 
-        //get neighbors
-        molec_get_neighbors(CellNumberOne);
+        // get neighbors
+        molec_get_neighbors(CellNumberOne, &neighbors[0]);
 
-        //get my index list
-        int* IndexArrayOfAtomsInCellOne=molec_cellList[CellNumberOne];
+        // get my index list
+        int* IndexArrayOfAtomsInCellOne = molec_cellList[CellNumberOne];
 
         int NeighborNumber;
-        //loop over all neighbors
-        for(NeighborNumber=0;NeighborNumber<27;++NeighborNumber)
+        // loop over all neighbors
+        for(NeighborNumber = 0; NeighborNumber < 27; ++NeighborNumber)
         {
-        int CellNumberTwo=neighbors[NeighborNumber];
+            int CellNumberTwo = neighbors[NeighborNumber];
 
-        //get corresponding list
-        int* IndexArrayOfAtomsInCellTwo=molec_cellList[CellNumberTwo];
+            // get corresponding list
+            int* IndexArrayOfAtomsInCellTwo = molec_cellList[CellNumberTwo];
 
-        //case one for different cells
-        if(CellNumberOne<CellNumberTwo)
-        {
-            //calculate interaction with loop over the two lists
-            int k,i;
-            for(k=0;k<NumberOfAtomsInCell[CellNumberOne];++k)//first list
+            // case one for different cells
+            if(CellNumberOne < CellNumberTwo)
             {
-
-                //index one
-                int IndexOfAtomOne=IndexArrayOfAtomsInCellOne[k];
-
-                const Real xi = x[IndexOfAtomOne];
-                const Real yi = y[IndexOfAtomOne];
-                const Real zi = z[IndexOfAtomOne];
-
-                Real f_xi = f_x[IndexOfAtomOne];
-                Real f_yi = f_y[IndexOfAtomOne];
-                Real f_zi = f_z[IndexOfAtomOne];
-
-                for(i=0;i<NumberOfAtomsInCell[CellNumberTwo];++i)//second list
+                // calculate interaction with loop over the two lists
+                int k, i;
+                for(k = 0; k < NumberOfAtomsInCell[CellNumberOne]; ++k) // first list
                 {
 
-                    //index two
-                    int IndexOfAtomTwo=IndexArrayOfAtomsInCellTwo[i];
+                    // index one
+                    int IndexOfAtomOne = IndexArrayOfAtomsInCellOne[k];
 
-                    const Real xij = dist(xi, x[IndexOfAtomTwo], L);
-                    const Real yij = dist(yi, y[IndexOfAtomTwo], L);
-                    const Real zij = dist(zi, z[IndexOfAtomTwo], L);
+                    const Real xi = x[IndexOfAtomOne];
+                    const Real yi = y[IndexOfAtomOne];
+                    const Real zi = z[IndexOfAtomOne];
 
-                    const Real r2 = xij * xij + yij * yij + zij * zij;
+                    Real f_xi = f_x[IndexOfAtomOne];
+                    Real f_yi = f_y[IndexOfAtomOne];
+                    Real f_zi = f_z[IndexOfAtomOne];
 
-                    if(r2 < Rcut2)
+                    for(i = 0; i < NumberOfAtomsInCell[CellNumberTwo]; ++i) // second list
                     {
-                        // V(s) = 4 * eps * (s^12 - s^6) with  s = sig/r
-                        const Real s2 = (sigLJ * sigLJ) / r2;
-                        const Real s6 = s2 * s2 * s2;
 
-                        Epot_ += 4 * epsLJ * (s6 * s6 - s6);
+                        // index two
+                        int IndexOfAtomTwo = IndexArrayOfAtomsInCellTwo[i];
 
-                        const Real fr = 24 * epsLJ / r2 * (2 * s6 * s6 - s6);
+                        const Real xij = dist(xi, x[IndexOfAtomTwo], L);
+                        const Real yij = dist(yi, y[IndexOfAtomTwo], L);
+                        const Real zij = dist(zi, z[IndexOfAtomTwo], L);
 
-                        f_xi += fr * xij;
-                        f_yi += fr * yij;
-                        f_zi += fr * zij;
+                        const Real r2 = xij * xij + yij * yij + zij * zij;
 
-                        f_x[IndexOfAtomTwo] -= fr * xij;
-                        f_y[IndexOfAtomTwo] -= fr * yij;
-                        f_z[IndexOfAtomTwo] -= fr * zij;
-                    }
-                }//end second list
+                        if(r2 < Rcut2)
+                        {
+                            // V(s) = 4 * eps * (s^12 - s^6) with  s = sig/r
+                            const Real s2 = (sigLJ * sigLJ) / r2;
+                            const Real s6 = s2 * s2 * s2;
 
-                f_x[IndexOfAtomOne] = f_xi;
-                f_y[IndexOfAtomOne] = f_yi;
-                f_z[IndexOfAtomOne] = f_zi;
+                            Epot_ += 4 * epsLJ * (s6 * s6 - s6);
 
-             }//end first list
+                            const Real fr = 24 * epsLJ / r2 * (2 * s6 * s6 - s6);
 
-        }
-        else if(CellNumberOne==CellNumberTwo) //case two for the same cell
-        {
-            //calculate interaction with loop over the two lists
-            int k,i;
-            for(k=0;k<NumberOfAtomsInCell[CellNumberOne];++k)//first list
+                            f_xi += fr * xij;
+                            f_yi += fr * yij;
+                            f_zi += fr * zij;
+
+                            f_x[IndexOfAtomTwo] -= fr * xij;
+                            f_y[IndexOfAtomTwo] -= fr * yij;
+                            f_z[IndexOfAtomTwo] -= fr * zij;
+                        }
+                    } // end second list
+
+                    f_x[IndexOfAtomOne] = f_xi;
+                    f_y[IndexOfAtomOne] = f_yi;
+                    f_z[IndexOfAtomOne] = f_zi;
+
+                } // end first list
+            }
+            else if(CellNumberOne == CellNumberTwo) // case two for the same cell
             {
-                //index one
-                int IndexOfAtomOne=IndexArrayOfAtomsInCellOne[k];
-
-                const Real xi = x[IndexOfAtomOne];
-                const Real yi = y[IndexOfAtomOne];
-                const Real zi = z[IndexOfAtomOne];
-
-                Real f_xi = f_x[IndexOfAtomOne];
-                Real f_yi = f_y[IndexOfAtomOne];
-                Real f_zi = f_z[IndexOfAtomOne];
-
-                for(i=k+1;i<NumberOfAtomsInCell[CellNumberTwo];++i)//second list
+                // calculate interaction with loop over the two lists
+                int k, i;
+                for(k = 0; k < NumberOfAtomsInCell[CellNumberOne]; ++k) // first list
                 {
-                    //index two
-                    int IndexOfAtomTwo=IndexArrayOfAtomsInCellTwo[i];
+                    // index one
+                    int IndexOfAtomOne = IndexArrayOfAtomsInCellOne[k];
 
-                    const Real xij = dist(xi, x[IndexOfAtomTwo], L);
-                    const Real yij = dist(yi, y[IndexOfAtomTwo], L);
-                    const Real zij = dist(zi, z[IndexOfAtomTwo], L);
+                    const Real xi = x[IndexOfAtomOne];
+                    const Real yi = y[IndexOfAtomOne];
+                    const Real zi = z[IndexOfAtomOne];
 
-                    const Real r2 = xij * xij + yij * yij + zij * zij;
+                    Real f_xi = f_x[IndexOfAtomOne];
+                    Real f_yi = f_y[IndexOfAtomOne];
+                    Real f_zi = f_z[IndexOfAtomOne];
 
-                    if(r2 < Rcut2)
+                    for(i = k + 1; i < NumberOfAtomsInCell[CellNumberTwo]; ++i) // second list
                     {
-                        // V(s) = 4 * eps * (s^12 - s^6) with  s = sig/r
-                        const Real s2 = (sigLJ * sigLJ) / r2;
-                        const Real s6 = s2 * s2 * s2;
+                        // index two
+                        int IndexOfAtomTwo = IndexArrayOfAtomsInCellTwo[i];
 
-                        Epot_ += 4 * epsLJ * (s6 * s6 - s6);
+                        const Real xij = dist(xi, x[IndexOfAtomTwo], L);
+                        const Real yij = dist(yi, y[IndexOfAtomTwo], L);
+                        const Real zij = dist(zi, z[IndexOfAtomTwo], L);
 
-                        const Real fr = 24 * epsLJ / r2 * (2 * s6 * s6 - s6);
+                        const Real r2 = xij * xij + yij * yij + zij * zij;
 
-                        f_xi += fr * xij;
-                        f_yi += fr * yij;
-                        f_zi += fr * zij;
+                        if(r2 < Rcut2)
+                        {
+                            // V(s) = 4 * eps * (s^12 - s^6) with  s = sig/r
+                            const Real s2 = (sigLJ * sigLJ) / r2;
+                            const Real s6 = s2 * s2 * s2;
 
-                        f_x[IndexOfAtomTwo] -= fr * xij;
-                        f_y[IndexOfAtomTwo] -= fr * yij;
-                        f_z[IndexOfAtomTwo] -= fr * zij;
+                            Epot_ += 4 * epsLJ * (s6 * s6 - s6);
 
-                    }//end if
+                            const Real fr = 24 * epsLJ / r2 * (2 * s6 * s6 - s6);
 
-                }//end second list
+                            f_xi += fr * xij;
+                            f_yi += fr * yij;
+                            f_zi += fr * zij;
 
-                f_x[IndexOfAtomOne] = f_xi;
-                f_y[IndexOfAtomOne] = f_yi;
-                f_z[IndexOfAtomOne] = f_zi;
+                            f_x[IndexOfAtomTwo] -= fr * xij;
+                            f_y[IndexOfAtomTwo] -= fr * yij;
+                            f_z[IndexOfAtomTwo] -= fr * zij;
 
-                }//end first list
+                        } // end if
 
-            }//end else
+                    } // end second list
 
-        }//end neighbors
+                    f_x[IndexOfAtomOne] = f_xi;
+                    f_y[IndexOfAtomOne] = f_yi;
+                    f_z[IndexOfAtomOne] = f_zi;
 
-        }//end loop cells
+                } // end first list
+
+            } // end else
+
+        } // end neighbors
+
+    } // end loop cells
 
     *Epot = Epot_;
 }
